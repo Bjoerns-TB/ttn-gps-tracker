0c06f5c7fb47f5f3dc8191b804460abd478cc978
Bjoerns-TB <30447739+Bjoerns-TB@users.noreply.github.com>
2017-10-13 14:21:10 +0200

    Update tracker.ino
    Add contributions

--- source/tracker.ino
+++ source/tracker.ino
@@ -1,3 +1,6 @@
+//This is the first commit of an Arduino Sketch for an GPS-Tracker to work with The Things Network.
+//This Sketch uses TinyGPS, the Arduino LMIC-Library by Matthijs Kooijmanand and the hartware serial of an Adafruit Feater LoRa.
+//I use sample Code from Pieter Hensen https://github.com/Teumaat/ttn-tracker
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
