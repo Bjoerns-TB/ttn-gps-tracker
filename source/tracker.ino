ae5b327c6bf75b235f3eeb1ab1637ae7bedbb17e
Björn Amann <30447739+Bjoerns-TB@users.noreply.github.com>
2019-10-24 10:05:32 +0200

    Updated to TinyGPSPlus

--- source/tracker.ino
+++ source/tracker.ino
@@ -1,12 +1,14 @@
-//This is the first commit of an Arduino Sketch for an GPS-Tracker to work with The Things Network.
-//This Sketch uses TinyGPS, the Arduino LMIC-Library by Matthijs Kooijmanand and the hartware serial of an Adafruit Feater LoRa.
-//I use sample Code from Pieter Hensen https://github.com/Teumaat/ttn-tracker
+//An Arduino Sketch for an GPS-Tracker to work with The Things Network.
+//This Sketch uses TinyGPSPlus https://github.com/mikalhart/TinyGPSPlus
+//and the Arduino LMIC-Library by MCCI Catena https://github.com/mcci-catena/arduino-lmic
+//The hardware used is an Afafruit Feather 32u4 Lora with the Ultimate GPS featherwing.
+
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
-#include <TinyGPS.h>
+#include <TinyGPS++.h>
 
-TinyGPS gps;
+TinyGPSPlus gps;
 
 static const PROGMEM u1_t NWKSKEY[16] = { 0xE7, 0x11, 0xA6, 0xFD, 0xE2, 0x1B, 0x60, 0x6F, 0x65, 0x0C, 0x8B, 0x26, 0x05, 0xBB, 0xAA, 0xAA };
 static const u1_t PROGMEM APPSKEY[16] = { 0x71, 0x68, 0x53, 0xE9, 0x4C, 0x9A, 0x08, 0xED, 0xD0, 0x14, 0x00, 0xEE, 0xED, 0xBB, 0xBB, 0xBB };
@@ -17,10 +19,13 @@ void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
 uint8_t coords[9];
+uint32_t LatitudeBinary, LongitudeBinary;
+uint16_t altitudeGps;
+uint8_t hdopGps;
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty cycle limitations).
-const unsigned TX_INTERVAL = 60;
+const unsigned TX_INTERVAL = 30;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
@@ -31,51 +36,36 @@ const lmic_pinmap lmic_pins = {
 };
 
 void get_coords () {
-  bool newData = false;
-  unsigned long chars;
-  unsigned short sentences, failed;
-  float flat, flon, faltitudeGPS, fhdopGPS;
-  unsigned long age;
 
   // For one second we parse GPS data and report some key values
   for (unsigned long start = millis(); millis() - start < 1000;) {
     while (Serial1.available()) {
       char c = Serial1.read();
-      Serial.write(c); // uncomment this line if you want to see the GPS data flowing
-      if (gps.encode(c)) { // Did a new valid sentence come in?
-        newData = true;
-      }
+      //Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      gps.encode(c);
     }
   }
 
-  if ( newData ) {
-    gps.f_get_position(&flat, &flon, &age);
-    faltitudeGPS = gps.f_altitude();
-    fhdopGPS = gps.hdop();
-    flat = (flat == TinyGPS::GPS_INVALID_F_ANGLE ) ? 0.0 : flat;
-    flon = (flon == TinyGPS::GPS_INVALID_F_ANGLE ) ? 0.0 : flon;
+  if (gps.location.isValid()) {
+
+    LatitudeBinary = ((gps.location.lat() + 90) / 180.0) * 16777215;
+    LongitudeBinary = ((gps.location.lng() + 180) / 360.0) * 16777215;
+
+    coords[0] = ( LatitudeBinary >> 16 ) & 0xFF;
+    coords[1] = ( LatitudeBinary >> 8 ) & 0xFF;
+    coords[2] = LatitudeBinary & 0xFF;
+
+    coords[3] = ( LongitudeBinary >> 16 ) & 0xFF;
+    coords[4] = ( LongitudeBinary >> 8 ) & 0xFF;
+    coords[5] = LongitudeBinary & 0xFF;
+
+    altitudeGps = gps.altitude.meters();
+    coords[6] = ( altitudeGps >> 8 ) & 0xFF;
+    coords[7] = altitudeGps & 0xFF;
+
+    hdopGps = gps.hdop.value() / 10;
+    coords[8] = hdopGps & 0xFF;
   }
-
-  gps.stats(&chars, &sentences, &failed);
-
-  int32_t lat = flat * 10000;
-  int32_t lon = flon * 10000;
-  int16_t altitudeGPS = faltitudeGPS * 100;
-  int8_t hdopGPS = fhdopGPS; 
-
-  // Pad 2 int32_t to 6 8uint_t, big endian (24 bit each, having 11 meter precision)
-  coords[0] = lat;
-  coords[1] = lat >> 8;
-  coords[2] = lat >> 16;
-
-  coords[3] = lon;
-  coords[4] = lon >> 8;
-  coords[5] = lon >> 16;
-  
-  coords[6] = altitudeGPS;
-  coords[7] = altitudeGPS >> 8;
-
-  coords[8] = hdopGPS;
 }
 
 void do_send(osjob_t* j) {
@@ -94,7 +84,7 @@ void do_send(osjob_t* j) {
 void onEvent (ev_t ev) {
   Serial.print(os_getTime());
   Serial.print(": ");
-  switch(ev) {
+  switch (ev) {
     case EV_SCAN_TIMEOUT:
       Serial.println(F("EV_SCAN_TIMEOUT"));
       break;
@@ -117,9 +107,6 @@ void onEvent (ev_t ev) {
       // during join, but not supported by TTN at this time).
       LMIC_setLinkCheckMode(0);
       break;
-    case EV_RFU1:
-      Serial.println(F("EV_RFU1"));
-      break;
     case EV_JOIN_FAILED:
       Serial.println(F("EV_JOIN_FAILED"));
       break;
@@ -137,7 +124,7 @@ void onEvent (ev_t ev) {
         Serial.println(F(" bytes of payload"));
       }
       // Schedule next transmission
-      os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
       break;
     case EV_LOST_TSYNC:
       Serial.println(F("EV_LOST_TSYNC"));
@@ -155,6 +142,9 @@ void onEvent (ev_t ev) {
     case EV_LINK_ALIVE:
       Serial.println(F("EV_LINK_ALIVE"));
       break;
+    case EV_TXSTART:
+      Serial.println(F("EV_TXSTART"));
+      break;
     default:
       Serial.println(F("Unknown event"));
       break;
@@ -167,11 +157,10 @@ void setup()
   Serial.println(F("Starting"));
 
   Serial1.begin(9600);
-  
   delay(5000);
-  Serial1.print("$PMTK313,1*2E\r\n");  // Enable to search a SBAS satellite
   Serial1.print("$PMTK301,2*2E\r\n");  // Select SBAS as DGPS source (RTCM)
-  Serial1.print("$PMTK513,1*28\r\n");
+  Serial1.print("$PMTK313,1*2E\r\n");  // Enable to search a SBAS satellite
+  //Serial1.print("$PMTK513,1*28\r\n");
 
   // LMIC init
   os_init();
@@ -179,64 +168,64 @@ void setup()
   // Reset the MAC state. Session and pending data transfers will be discarded.
   LMIC_reset();
 
-    // Set static session parameters. Instead of dynamically establishing a session
-    // by joining the network, precomputed session parameters are be provided.
-    #ifdef PROGMEM
-    // On AVR, these values are stored in flash and only copied to RAM
-    // once. Copy them to a temporary buffer here, LMIC_setSession will
-    // copy them into a buffer of its own again.
-    uint8_t appskey[sizeof(APPSKEY)];
-    uint8_t nwkskey[sizeof(NWKSKEY)];
-    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
-    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
-    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
-    #else
-    // If not running an AVR with PROGMEM, just use the arrays directly
-    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
-    #endif
+  // Set static session parameters. Instead of dynamically establishing a session
+  // by joining the network, precomputed session parameters are be provided.
+#ifdef PROGMEM
+  // On AVR, these values are stored in flash and only copied to RAM
+  // once. Copy them to a temporary buffer here, LMIC_setSession will
+  // copy them into a buffer of its own again.
+  uint8_t appskey[sizeof(APPSKEY)];
+  uint8_t nwkskey[sizeof(NWKSKEY)];
+  memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+  memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+  LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+#else
+  // If not running an AVR with PROGMEM, just use the arrays directly
+  LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+#endif
 
-    #if defined(CFG_eu868)
-    // Set up the channels used by the Things Network, which corresponds
-    // to the defaults of most gateways. Without this, only three base
-    // channels from the LoRaWAN specification are used, which certainly
-    // works, so it is good for debugging, but can overload those
-    // frequencies, so be sure to configure the full frequency range of
-    // your network here (unless your network autoconfigures them).
-    // Setting up channels should happen after LMIC_setSession, as that
-    // configures the minimal channel set.
-    // NA-US channels 0-71 are configured automatically
-    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
-    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    // LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    // LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    // LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    // LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    // LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    // LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
-    // devices' ping slots. LMIC does not have an easy way to define set this
-    // frequency and support for class B is spotty and untested, so this
-    // frequency is not configured here.
-    #elif defined(CFG_us915)
-    // NA-US channels 0-71 are configured automatically
-    // but only one group of 8 should (a subband) should be active
-    // TTN recommends the second sub band, 1 in a zero based count.
-    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
-    LMIC_selectSubBand(1);
-    #endif
+#if defined(CFG_eu868)
+  // Set up the channels used by the Things Network, which corresponds
+  // to the defaults of most gateways. Without this, only three base
+  // channels from the LoRaWAN specification are used, which certainly
+  // works, so it is good for debugging, but can overload those
+  // frequencies, so be sure to configure the full frequency range of
+  // your network here (unless your network autoconfigures them).
+  // Setting up channels should happen after LMIC_setSession, as that
+  // configures the minimal channel set.
+  // NA-US channels 0-71 are configured automatically
+  LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+  LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  // LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  // LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  // LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  // LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  // LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  // LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+  // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+  // devices' ping slots. LMIC does not have an easy way to define set this
+  // frequency and support for class B is spotty and untested, so this
+  // frequency is not configured here.
+#elif defined(CFG_us915)
+  // NA-US channels 0-71 are configured automatically
+  // but only one group of 8 should (a subband) should be active
+  // TTN recommends the second sub band, 1 in a zero based count.
+  // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+  LMIC_selectSubBand(1);
+#endif
 
-    // Disable link check validation
-    LMIC_setLinkCheckMode(0);
+  // Disable link check validation
+  LMIC_setLinkCheckMode(0);
 
-    // TTN uses SF9 for its RX2 window.
-    LMIC.dn2Dr = DR_SF9;
+  // TTN uses SF9 for its RX2 window.
+  LMIC.dn2Dr = DR_SF9;
 
-    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(DR_SF7,14);
+  // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+  LMIC_setDrTxpow(DR_SF7, 14);
 
-    // Start job
-    do_send(&sendjob);
+  // Start job
+  do_send(&sendjob);
 }
 
 void loop() {
